e70037734ec84061a192e835b1dfc908ace450f4
Sebastian Werner <s.werner@sebastian-software.de>
2017-06-02 22:25:26 +0200

    Fixed jest API for hash tests

--- src/lib/hash.test.js
+++ src/lib/hash.test.js
@@ -5,7 +5,7 @@ describe('hash', () => {
     const fileContent = fs.readFileSync('test/fixtures/pixel.gif');
 
     it('should calc hash with default params (xxhash32 and shrink=8)', () => {
-        assert.equal(calcHash(fileContent), 'fb27d692');
+        expect(calcHash(fileContent)).toBe('fb27d692');
     });
 
     it('should calc hash (xxhash64 and shrink=16)', () => {
@@ -14,7 +14,7 @@ describe('hash', () => {
             shrink: 16
         };
 
-        assert.equal(calcHash(fileContent, options), '56ed89bfa97a733e');
+        expect(calcHash(fileContent, options)).toBe('56ed89bfa97a733e');
     });
 
     it('should calc hash with custom function', () => {
@@ -23,7 +23,7 @@ describe('hash', () => {
             shrink: 3
         };
 
-        assert.equal(calcHash(fileContent, options), '123');
+        expect(calcHash(fileContent, options)).toBe('123');
     });
 
     it('should calc hash with crypto method', () => {
@@ -32,6 +32,6 @@ describe('hash', () => {
             shrink: 16
         };
 
-        assert.equal(calcHash(fileContent, options), '09d46019c7a75b96');
+        expect(calcHash(fileContent, options)).toBe('09d46019c7a75b96');
     });
 });

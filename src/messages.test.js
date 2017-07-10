b135a74e1208d70816afbeb4e210ca5e761f4135
Sebastian Werner <s.werner@sebastian-software.de>
2017-07-10 14:50:54 +0200

    Using async fixture reader

--- src/messages.test.js
+++ src/messages.test.js
@@ -1,40 +1,41 @@
 import path from "path"
 import postcss from "postcss"
 import postcssUrl from "."
-import { read } from "../test/setup"
+import { readAsync } from "../test/setup"
 
 describe("misc", () => {
-  test("should add dependency messages with copy", () => {
-    return postcss()
+  test("should add dependency messages with copy", () =>
+    readAsync("copy").then((value) => postcss()
       .use(
         postcssUrl({
           url: "copy",
           assetsPath: "test/fixtures/build/assets"
         })
       )
-      .process(read("copy"), {
+      .process(value, {
         from: "test/fixtures/copy.css"
       })
-      .then((result) => {
-        const dependencies = result.messages.filter((m) => m.type === "dependency")
-        expect(dependencies).toMatchSnapshot()
-      })
-  })
+    ).then((result) => {
+      const dependencies = result.messages.filter((message) => message.type === "dependency")
+      expect(dependencies).toMatchSnapshot()
+    })
+  )
 
-  test("should add dependency messages with inline", () => {
-    return postcss()
+  test("should add dependency messages with inline", () =>
+    readAsync("copy").then((value) => postcss()
       .use(
         postcssUrl({
           url: "inline",
           assetsPath: "test/fixtures/build/assets"
         })
       )
-      .process(read("copy"), {
+      .process(value, {
         from: "test/fixtures/copy.css"
       })
       .then((result) => {
-        const dependencies = result.messages.filter((m) => m.type === "dependency")
+        const dependencies = result.messages.filter((message) => message.type === "dependency")
         expect(dependencies).toMatchSnapshot()
       })
-  })
+    )
+  )
 })

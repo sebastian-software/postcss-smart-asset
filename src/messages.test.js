a39c37240318a89cd8f66ab3b53fcc9285f2549e
Sebastian Werner <s.werner@sebastian-software.de>
2017-07-10 14:52:10 +0200

    Auto formatting

--- src/messages.test.js
+++ src/messages.test.js
@@ -5,37 +5,39 @@ import { readAsync } from "../test/setup"
 
 describe("misc", () => {
   test("should add dependency messages with copy", () =>
-    readAsync("copy").then((value) => postcss()
-      .use(
-        postcssUrl({
-          url: "copy",
-          assetsPath: "test/fixtures/build/assets"
-        })
+    readAsync("copy")
+      .then((value) =>
+        postcss()
+          .use(
+            postcssUrl({
+              url: "copy",
+              assetsPath: "test/fixtures/build/assets"
+            })
+          )
+          .process(value, {
+            from: "test/fixtures/copy.css"
+          })
       )
-      .process(value, {
-        from: "test/fixtures/copy.css"
-      })
-    ).then((result) => {
-      const dependencies = result.messages.filter((message) => message.type === "dependency")
-      expect(dependencies).toMatchSnapshot()
-    })
-  )
-
-  test("should add dependency messages with inline", () =>
-    readAsync("copy").then((value) => postcss()
-      .use(
-        postcssUrl({
-          url: "inline",
-          assetsPath: "test/fixtures/build/assets"
-        })
-      )
-      .process(value, {
-        from: "test/fixtures/copy.css"
-      })
       .then((result) => {
         const dependencies = result.messages.filter((message) => message.type === "dependency")
         expect(dependencies).toMatchSnapshot()
-      })
-    )
-  )
+      }))
+
+  test("should add dependency messages with inline", () =>
+    readAsync("copy").then((value) =>
+      postcss()
+        .use(
+          postcssUrl({
+            url: "inline",
+            assetsPath: "test/fixtures/build/assets"
+          })
+        )
+        .process(value, {
+          from: "test/fixtures/copy.css"
+        })
+        .then((result) => {
+          const dependencies = result.messages.filter((message) => message.type === "dependency")
+          expect(dependencies).toMatchSnapshot()
+        })
+    ))
 })

cfb39a2c4a1981dfd579a3b49794b164719e315a
Sebastian Werner <s.werner@sebastian-software.de>
2017-07-11 17:30:08 +0200

    Respect enforced relative URLs when written by the user to be translated into relative urls as well. For Webpack compat.

--- src/type/rebase.js
+++ src/type/rebase.js
@@ -11,7 +11,15 @@ import { normalize } from "../lib/paths"
  * @returns {String|Undefined}
  */
 export default function(asset, dir) {
-  const rebasedUrl = normalize(path.relative(dir.to, asset.absolutePath))
+  let rebasedUrl = normalize(path.relative(dir.to, asset.absolutePath))
+
+  // For Webpack compatibility as of v2.0.0 to v3.1.0 we need to prepend a relative path indicator.
+  // Otherwise Webpack tries to interpret the url as a package from node_modules which is probably
+  // no what we meant it to be.
+  let wasExplicitelyLocal = asset.originUrl.charAt(0) === "." || asset.originUrl.charAt(0) === "/"
+  if (wasExplicitelyLocal && rebasedUrl.charAt(0) !== "." && rebasedUrl.charAt(0) !== "/") {
+    rebasedUrl = `./${rebasedUrl}`
+  }
 
   return `${rebasedUrl}${asset.search}${asset.hash}`
 }
